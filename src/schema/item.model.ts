aea299ea31fad78b422fad0cb5701e71d1848bcb
Twitteryzeus <bvlakkad@gmail.com>
2022-11-05 21:38:37 +0530

    refactor mongoDB code

--- src/schema/item.model.ts
+++ src/schema/item.model.ts
@@ -1,11 +1,18 @@
 import mongoose from "./index";
 
 const todoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: false },
-},
-  { timestamps: true }
-);
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String, 
+    required: false
+  }
+},{
+  timestamps: true
+});
 
-// Student model
 const Todo = mongoose.model("Todo", todoSchema);
+
+export { Todo };
