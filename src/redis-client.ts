3c088bea8fb72031a4bc9773bcdee515e2bbccad
Twitteryzeus <bvlakkad@gmail.com>
2022-11-06 15:53:58 +0530

    add get-set method for redis

--- src/redis-client.ts
+++ src/redis-client.ts
@@ -9,4 +9,24 @@ const redisClient = redis.createClient({
   password: envConfigs.redis.password,
 });
 
-export default redisClient;
+const set = async (key: string = 'BACKEND_TASK_BRIJESH', value: string = '') => {
+  try {
+    await redisClient.set(key, value);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const get = async (key = 'BACKEND_TASK_BRIJESH') => {
+  try {
+    const value = await redisClient.get(key);
+    return value;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export default { set, get };
